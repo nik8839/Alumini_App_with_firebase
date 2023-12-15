fbaf3277600458080e290e72c61dca341c5d0fb9
nik8839 <128407184+nik8839@users.noreply.github.com>
2023-12-15 19:03:49 +0530

    Merge remote-tracking branch 'origin/master'

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
-# appdev
+## This is a course project under the course android development. 
+-----------------------------------------------------------------------------------------------
 
-A new Flutter project.
+## This app is a simple alumini app , that basically has simple login,sign-up page. The user has to first signup the page before login for first time. User are of two categories admin and alumini.
 
-## Getting Started
+---------------------------------------------------------------------------------------------------------
+## Admin can also login to the account and can delete the existing user on the home page. As of now only one functionality has been implemented.
 
-This project is a starting point for a Flutter application.
+-------------------------------------------------------------------------------------------------------
 
-A few resources to get you started if this is your first Flutter project:
+## Alumini after login can see other alumini if existing. They can chat with any of the alumini and can exchange the messages.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+------------------------------------------------------------------------------------------------------
+## This simple project basically highlights the use of firebase for authentication and using cloud firestore to store the chats.
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+------------------------------------------------------------------------------------------------------
+
+## To use the project one can directly clone it, updating pubspec.yaml file by adding firebase,cloud firestore and firebase_core with the latest version.
+
+--------------------------------------------------------------------------------------------------------
+## Then use the following commands in flutter terminal 
+# firebase login
+# dart pub global activate flutterfire_cli
+# flutterfire configure
+It will ask to select project from firebase project list
+-------------------------------------------------------------------------------------------------------
+## update pugins and classpath in build.gradle file and finally use flutter run after selecting emulator.
+-----------------------------------------------------------------------------------------------------------
+## For more detailed steps one can directly see firebase doc for easy setup.
+-------------------------------------------------------------------------------------------------------
+
--- lib/pages/alumini_connect_page.dart
+++ lib/pages/alumini_connect_page.dart
@@ -3,21 +3,15 @@ import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-import 'chatpage.dart'; // Import the dart:math library
-
-// ...
+import 'chatpage.dart';
 
 class AlumniConnectPage extends StatelessWidget {
   final String currentUserUid;
 
   const AlumniConnectPage({Key? key, required this.currentUserUid}) : super(key: key);
 
-  // Function to generate a unique chat room ID
   String generateChatRoomId(String user1, String user2) {
-    // Sort the user IDs to ensure consistency
     List<String> users = [user1, user2]..sort();
-
-    // Concatenate and return the sorted user IDs
     return "${users[0]}_${users[1]}";
   }
 
@@ -25,7 +19,7 @@ class AlumniConnectPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Alumini ')),
+        title: Center(child: Text('Alumni ')),
       ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance.collection('users').snapshots(),
@@ -44,17 +38,43 @@ class AlumniConnectPage extends StatelessWidget {
               final alumniUser = alumni[index];
               String chatRoomId = generateChatRoomId(currentUserUid, alumniUser['uid']);
 
-              return ListTile(
-                title: Text(alumniUser['displayName']),
-                subtitle: Text('Graduation Year: ${alumniUser['graduationYear']}\nCurrent Job: ${alumniUser['currentJob']}'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ChatPage(chatRoomId: chatRoomId),
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 2),
                     ),
-                  );
-                },
+                  ],
+                ),
+                child: ListTile(
+                  title: Text(
+                    alumniUser['displayName'],
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Graduation Year: ${alumniUser['graduationYear']}\nCurrent Job: ${alumniUser['currentJob']}',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatPage(chatRoomId: chatRoomId),
+                      ),
+                    );
+                  },
+                ),
               );
             },
           );
